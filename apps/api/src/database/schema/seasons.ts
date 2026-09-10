7f57a38c2bd7e7ff6eb01e064bcd3f4017382700
Shivang Patel <shivangpatel2050@gmail.com>
2026-09-10 19:35:05 -0400

    feat: create better auth client and generate schema with drizzle-orm (#7)
    * in progress as of now i addedthe env made dockercompose.yaml and set up docker and even amde a drizzlle gernation but there are issues in the migration
    
    * Configure Turbo for API builds (#5)
    
    * still need to work on adding the database commands andfiguring out if prisma is the plan
    
    * fixed the turbo.json for nest
    
    * added next-env.d.ts to the env
    
    * feat: write database/schema folder with modular design and update routing configs
    
    * discord deps (since package deps didnt push earlier, some of these won't be used yet and file might be inaccurate)
    
    * feat: add better auth config + client file and auto generate schema
    
    * fix: remove unused imports in schema declarations
    
    ---------
    
    Co-authored-by: anishb77 <anishboddu7@gmail.com>

---
+++ apps/api/src/database/schema/seasons.ts
@@ -0,0 +1,20 @@
+import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { memberStatus } from "./enums";
+import { user } from "./auth";
+
+export const seasons = pgTable("seasons", {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    started_at: timestamp("started_at").notNull(),
+    ended_at: timestamp("ended_at").notNull()
+});
+
+export const season_memberships = pgTable("season_memberships", {
+    id: serial("id").primaryKey(),
+    season_id: integer("season_id").references(() => seasons.id),
+    user_id: text("user_id").references(() => user.id),
+    status: memberStatus("status").notNull(),
+    started_at: timestamp("started_at").defaultNow().notNull(),
+    ended_at: timestamp("ended_at"),
+    status_override: text("status_override")
+});
