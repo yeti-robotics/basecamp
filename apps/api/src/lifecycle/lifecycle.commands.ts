bb8c2acbee3fa4c6677ecd6b7358add25d167cb6
anishb77 <anishboddu7@gmail.com>
2026-09-13 20:42:20 -0400

    feat: implement logic for db greet function

--- apps/api/src/lifecycle/lifecycle.commands.ts
+++ apps/api/src/lifecycle/lifecycle.commands.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { MessageFlags } from "discord.js";
 import { Context, SlashCommand, type SlashCommandContext } from "necord";
+import { DatabaseService } from "./lifecycle.service.js";
 
 @Injectable()
 export class LifecycleCommands {
@@ -12,6 +13,27 @@ export class LifecycleCommands {
   public async onPing(@Context() [interaction]: SlashCommandContext) {
     return interaction.reply({
       content: `Pong! ${interaction.client.ws.ping}ms`,
+      flags: [MessageFlags.Ephemeral],
+    });
+  }
+
+  @SlashCommand({
+    name: "greet",
+    description: "Talk to the database",
+    dmPermission: true,
+  })
+  public async onGreet(@Context() [interaction]: SlashCommandContext) {
+    const connected = await new DatabaseService().checkConnection();
+
+    if (connected) {
+      return interaction.reply({
+        content: `Hello, ${interaction.user.username}! The database connection is working.`,
+        flags: [MessageFlags.Ephemeral],
+      });
+    }
+
+    return interaction.reply({
+      content: `Hello, ${interaction.user.username}! The database connection is not working.`,
       flags: [MessageFlags.Ephemeral],
     });
   }
--- apps/api/src/lifecycle/lifecycle.service.ts
+++ apps/api/src/lifecycle/lifecycle.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { ActivityType } from "discord.js";
+import { sql } from "drizzle-orm/sql/sql";
 import { Context, type ContextOf, Once } from "necord";
+import { db } from "src/auth.js";
 
 @Injectable()
 export class LifecycleService {
@@ -11,5 +13,17 @@ export class LifecycleService {
     this.logger.log(`Bot logged in as ${client.user.username}`);
 
     client.user.setActivity("YETI ARE YOU READY?", { type: ActivityType.Custom });
+  }
+}
+
+@Injectable()
+export class DatabaseService {
+  async checkConnection(): Promise<boolean> {
+    try {
+      await db.execute(sql`SELECT 1`);
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
