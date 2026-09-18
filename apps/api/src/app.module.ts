3e90dea3026526a17e4d173092a3bb37ab63b63c
anishb77 <anishboddu7@gmail.com>
2026-09-18 16:00:26 -0400

    initial commit to open draft pr

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,9 +6,22 @@ import { HandbookModule } from './handbook/handbook.module.js';
 import { LifecycleModule } from './lifecycle/lifecycle.module.js';
 import { OutreachModule } from './outreach/outreach.module.js';
 import { HealthModule } from './health/health.module.js';
+import { IntentsBitField } from 'discord.js';
+import { NecordModule } from 'necord';
 
 @Module({
-  imports: [AttendanceModule, LifecycleModule, HandbookModule, OutreachModule, HealthModule],
+  imports: [
+    NecordModule.forRoot({
+            token: process.env.DISCORD_BOT_TOKEN!,
+            intents: [ IntentsBitField.Flags.Guilds],
+            development: [process.env.DISCORD_DEVELOPMENT_GUILD_ID!]
+        }),
+    AttendanceModule,
+    LifecycleModule,
+    HandbookModule,
+    OutreachModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
