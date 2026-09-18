d1bf17bce343c9b7c7936ef5cf38a03e5b40b00c
Anish Boddu <anishboddu7@gmail.com>
2026-09-18 17:50:37 -0400

    task: wire necord and set up codebase for dev server (#19)
    * initial commit to open draft pr
    
    * task: add for root module behavior with env injection
    
    * refactor: proper naming convention for token

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,9 +6,31 @@ import { HandbookModule } from './handbook/handbook.module.js';
 import { LifecycleModule } from './lifecycle/lifecycle.module.js';
 import { OutreachModule } from './outreach/outreach.module.js';
 import { HealthModule } from './health/health.module.js';
+import { IntentsBitField } from 'discord.js';
+import { NecordModule } from 'necord';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AttendanceModule, LifecycleModule, HandbookModule, OutreachModule, HealthModule],
+  imports: [
+      NecordModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        token: config.getOrThrow<string>('DISCORD_TOKEN'),
+        intents: [IntentsBitField.Flags.Guilds],
+        development: [config.getOrThrow<string>('DISCORD_DEVELOPMENT_GUILD_ID')]
+      })
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.local']
+    }),
+    AttendanceModule,
+    LifecycleModule,
+    HandbookModule,
+    OutreachModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
